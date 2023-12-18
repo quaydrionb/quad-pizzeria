3e73a2f264e02ea1efaf1e7d4393a33eeef4f0eb
Quaydrion Butler <99278263+quaydrionb@users.noreply.github.com>
2023-12-18 14:36:55 -0500

    finished menu.html
    updated menu to have items

--- src/data.js
+++ src/data.js
@@ -24,3 +24,66 @@ let shopSpecialItemsData = [
     img: "./image/speciality-wings.jpeg",
   },
 ];
+
+let shopMenuItemsData = [
+  {
+    id: "dsjkads",
+    name: "Supreme Pizza",
+    price: 17.99,
+    size: "Medium",
+    desc: "Bold tomato sauce, melted mozzarella, savory pepperoni, sautéed mushrooms, colorful bell peppers, and sausage.",
+    img: "./image/supreme_pizza.jpeg",
+  },
+  {
+    id: "sfgrfgdh",
+    name: "White Veggie Pizza",
+    price: 15.99,
+    size: "Medium",
+    desc: "Featuring velvety white sauce, sharp cheddar, fresh tomatoes, crisp broccoli, and spinach leaves, all harmonized by melted mozzarella",
+    img: "./image/veggie pizza.JPG",
+  },
+  {
+    id: "fhfhddfh",
+    name: "Crispy Chicken",
+    price: 11.99,
+    size: "12 Wings",
+    desc: "Prepare your taste buds for a symphony of crunch with our irresistibly crispy chicken tenders",
+    img: ".image/crispy-chicken.jpeg",
+  },
+
+  {
+    id: "dfgdhfhj",
+    name: "Spicy Garlic Parmesan Wings",
+    price: 14.99,
+    size: "12 Wings",
+    desc: "A flavorful combination of spicy garlic and savory parmesan.",
+    img: "./image/spicy-garlic.jpeg",
+  },
+
+  {
+    id: "tjfhfdtyt",
+    name: "Rasberry Iced Tea",
+    price: 2.29,
+    size: "Regular",
+    desc: "Cool off with our delightful raspberry iced tea, available sweetened or unsweetened.",
+    img: "./image/iced-tea.jpeg",
+  },
+
+  {
+    id: "sdfdfhgfh",
+    name: "Cookies",
+    price: 1.29,
+    size: "2",
+    desc: "Tempting and chewy, our cookies are baked to perfection. Choose from classic chocolate chip or explore our seasonal flavors.",
+    img: "./image/cookies.jpeg",
+  },
+
+  {
+    id: "sdhjhkk",
+    name: "Brownies",
+    price: 3.24,
+    size: "2",
+    desc: "Indulge in the richness of our brownies, a perfect blend of fudgy goodness and sweet satisfaction.",
+    img: "./image/brownie.jpeg",
+  },
+];
